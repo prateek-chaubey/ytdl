b82a617d50308313b680e14bcfbed9bc36f9f6c4
Prateek Chaubey <41151614+prateek-chaubey@users.noreply.github.com>
2023-04-06 23:12:42 +0530

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,24 @@
+import express from "npm:express@^4.18.2";
+import * as ytdl from "https://deno.land/x/ytdl_core@v0.1.2/mod.ts";
+const app=express();
+
+app.get("/",(req,res)=>{
+res.sendFile("index.html",{ root: '.' });
+})
+
+app.get("/vid",async (req:any,res:any)=>{
+let id=req.query.id;
+if(ytdl.validateID(id)){
+
+let yt =await ytdl.getInfo(id); 
+res.json({"video":yt.player_response.videoDetails,"stream":yt.player_response.streamingData});
+}else{
+res.json({"error":true});
+}
+});
+
+
+
+
+
+app.listen(8000); 
